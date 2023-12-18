e2661fa8164bef6155ab31ba4019d8bd053c45d3
liyafei <liyafei@sensetime.com>
2023-12-18 19:18:06 +0800

    feat: app

--- app/options.go
+++ app/options.go
@@ -22,3 +22,45 @@ type options struct {
 	stopTimeout time.Duration
 	deliveries  []delivery.Delivery
 }
+
+func ID(id string) Option {
+	return func(o *options) {
+		o.id = id
+	}
+}
+
+func Name(name string) Option {
+	return func(o *options) {
+		o.name = name
+	}
+}
+
+func Version(version string) Option {
+	return func(o *options) {
+		o.version = version
+	}
+}
+
+func Metadata(md map[string]string) Option {
+	return func(o *options) {
+		o.metadata = md
+	}
+}
+
+func Deliveries(deliveries ...delivery.Delivery) Option {
+	return func(o *options) {
+		o.deliveries = deliveries
+	}
+}
+
+func Signal(sigs ...os.Signal) Option {
+	return func(o *options) {
+		o.sigs = sigs
+	}
+}
+
+func StopTimeout(t time.Duration) Option {
+	return func(o *options) {
+		o.stopTimeout = t
+	}
+}
